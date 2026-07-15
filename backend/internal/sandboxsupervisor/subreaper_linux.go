f9ea5705100f5f9c8e59041abd00488ab2c1c1a6
XianingY <byzaantios@gmail.com>
2026-07-15 09:10:31 +0800

    fix: clear platform security gates

--- backend/internal/sandboxsupervisor/subreaper_linux.go
+++ backend/internal/sandboxsupervisor/subreaper_linux.go
@@ -2,18 +2,33 @@
 
 package sandboxsupervisor
 
-import "golang.org/x/sys/unix"
+import (
+	"time"
+
+	"golang.org/x/sys/unix"
+)
 
 func enableSubreaper() error {
 	return unix.Prctl(unix.PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0)
 }
 
 func reapAdoptedChildren() {
+	deadline := time.Now().Add(2 * DefaultTermGrace)
 	for {
 		var status unix.WaitStatus
 		pid, err := unix.Wait4(-1, &status, unix.WNOHANG, nil)
-		if err != nil || pid <= 0 {
+		if err == unix.ECHILD {
 			return
 		}
+		if err != nil {
+			return
+		}
+		if pid > 0 {
+			continue
+		}
+		if time.Now().After(deadline) {
+			return
+		}
+		time.Sleep(10 * time.Millisecond)
 	}
 }
